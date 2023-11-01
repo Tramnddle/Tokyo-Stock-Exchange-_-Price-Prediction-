1e358db90a8feed228bfaa8d740aeab19be90b31
Tram <tram.nguyen.ngoc.nddle@gmail.com>
2023-11-01 11:08:39 +0100

    Update LSTM_py_script_streamlit.py

--- LSTM py script streamlit/LSTM_py_script_streamlit.py
+++ LSTM py script streamlit/LSTM_py_script_streamlit.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Install required packages from requirements.txt
-subprocess.call("pip install -r requirements.txt", shell=True)
+subprocess.call("pip install -r LSTM py script streamlit/requirements.txt", shell=True)
 
 import numpy as np
 import pandas as pd
