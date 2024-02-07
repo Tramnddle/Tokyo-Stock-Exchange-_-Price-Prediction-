967977e5b21a58deb066f43523f035771237cf43
Tram <tram.nguyen.ngoc.nddle@gmail.com>
2024-02-07 10:25:01 +0100

    Update LSTM_py_script_streamlit.py

--- LSTM_py_script_streamlit.py
+++ LSTM_py_script_streamlit.py
@@ -117,16 +117,20 @@ def df_to_windowed_df(dataframe, first_date_str, last_date_str, n, display_dataf
         return None
 
 def windowed_df_to_date_X_y(windowed_dataframe):
-  df_as_np = windowed_dataframe.to_numpy()
+    if windowed_dataframe is None:
+        return None, None, None
 
-  dates = df_as_np[:, 0]
+    df_as_np = windowed_dataframe.to_numpy()
 
-  middle_matrix = df_as_np[:, 1:-1]
-  X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
+    dates = df_as_np[:, 0]
 
-  Y = df_as_np[:, -1]
+    middle_matrix = df_as_np[:, 1:-1]
+    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
 
-  return dates, X.astype(np.float32), Y.astype(np.float32)
+    Y = df_as_np[:, -1]
+
+    return dates, X.astype(np.float32), Y.astype(np.float32)
+
 
 dates, X, y = windowed_df_to_date_X_y(df_to_windowed_df(data, '2020-12-03', '2021-12-03', n=5, display_dataframe=False))
 
