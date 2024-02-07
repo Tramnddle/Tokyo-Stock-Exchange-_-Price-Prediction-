0d4a23cfe466fa6d14234f66a64eb343c735b784
Tram <tram.nguyen.ngoc.nddle@gmail.com>
2024-02-07 11:07:44 +0100

    Update LSTM_py_script_streamlit.py

--- LSTM_py_script_streamlit.py
+++ LSTM_py_script_streamlit.py
@@ -62,54 +62,58 @@ def str_to_datetime(s):
   return datetime.datetime.strptime(str(s), '%Y-%m-%d')
 
 
-def df_to_windowed_df(dataframe, first_date_str, last_date_str, n, display_dataframe=True):
-    first_date = str_to_datetime(first_date_str)
-    last_date = str_to_datetime(last_date_str)
 
-    target_date = first_date
 
-    dates = []
-    X, Y = [], []
+def df_to_windowed_df(dataframe, first_date_str, last_date_str, n):
+  first_date = str_to_datetime(first_date_str)
+  last_date  = str_to_datetime(last_date_str)
 
-    last_time = False
-    while True:
-        df_subset = dataframe.loc[:target_date].tail(n + 1)
+  target_date = first_date
+  
+  dates = []
+  X, Y = [], []
 
-        if len(df_subset) != n + 1:
-            print(f'Error: Window of size {n} is too large for date {target_date}')
-            return
+  last_time = False
+  while True:
+    df_subset = dataframe.loc[:target_date].tail(n+1)
+    
+    if len(df_subset) != n+1:
+      print(f'Error: Window of size {n} is too large for date {target_date}')
+      return
 
-        values = df_subset['Close'].to_numpy()
-        x, y = values[:-1], values[-1]
+    values = df_subset['Close'].to_numpy()
+    x, y = values[:-1], values[-1]
 
-        dates.append(target_date)
-        X.append(x)
-        Y.append(y)
+    dates.append(target_date)
+    X.append(x)
+    Y.append(y)
 
-        next_week = dataframe.loc[target_date:target_date + datetime.timedelta(days=7)]
-        next_datetime_str = str(next_week.head(2).tail(1).index.values[0])
-        next_date_str = next_datetime_str.split('T')[0]
-        year_month_day = next_date_str.split('-')
-        year, month, day = year_month_day
-        next_date = datetime.datetime(day=int(day), month=int(month), year=int(year))
+    next_week = dataframe.loc[target_date:target_date+datetime.timedelta(days=7)]
+    next_datetime_str = str(next_week.head(2).tail(1).index.values[0])
+    next_date_str = next_datetime_str.split('T')[0]
+    year_month_day = next_date_str.split('-')
+    year, month, day = year_month_day
+    next_date = datetime.datetime(day=int(day), month=int(month), year=int(year))
+    
+    if last_time:
+      break
+    
+    target_date = next_date
 
-        if last_time:
-            break
+    if target_date == last_date:
+      last_time = True
+    
+  ret_df = pd.DataFrame({})
+  ret_df['Target Date'] = dates
+  
+  X = np.array(X)
+  for i in range(0, n):
+    X[:, i]
+    ret_df[f'Target-{n-i}'] = X[:, i]
+  
+  ret_df['Target'] = Y
 
-        target_date = next_date
-
-        if target_date == last_date:
-            last_time = True
-
-    ret_df = pd.DataFrame({})
-    ret_df['Target Date'] = dates
-
-    X = np.array(X)
-    for i in range(0, n):
-        X[:, i]
-        ret_df[f'Target-{n - i}'] = X[:, i]
-
-    ret_df['Target'] = Y
+  return ret_df
 
 
 def windowed_df_to_date_X_y(windowed_dataframe):
